8a587c686fe3623216b62d3a8edc5c8aba59d942
Ismael Blazquez Carabias <ismael.blazquez@edu.uah.es>
2025-09-16 01:56:07 +0200

    docker-compose y otros scritps y ficheros de configuracion/infraestructura para montar el entorno con todos los servicios en local

---
+++ role.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM tfm_users.role
+LIMIT 0, 1000
+
+-- Date: 2025-09-15 22:50
+*/
+INSERT INTO `tfm_users.role` (`id`,`description`,`name`) VALUES (1,'Administrador del sistema','ADMIN');
+INSERT INTO `tfm_users.role` (`id`,`description`,`name`) VALUES (2,'Usuario músico','MUSICIAN');
