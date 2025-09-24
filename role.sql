f4ec4e29c467a208e6221d690f152f2ba619cccf
Ismael Blazquez Carabias <ismael.blazquez@edu.uah.es>
2025-09-25 01:25:08 +0200

    Se añaden colecciones de postman

--- role.sql
+++
@@ -1,8 +0,0 @@
-/*
--- Query: SELECT * FROM tfm_users.role
-LIMIT 0, 1000
-
--- Date: 2025-09-15 22:50
-*/
-INSERT INTO `tfm_users.role` (`id`,`description`,`name`) VALUES (1,'Administrador del sistema','ADMIN');
-INSERT INTO `tfm_users.role` (`id`,`description`,`name`) VALUES (2,'Usuario músico','MUSICIAN');
